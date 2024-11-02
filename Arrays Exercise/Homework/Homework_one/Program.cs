0e0a664ed7e132f5548d2e9234156d1a810ed38d
Kalin1603 <kalintodorov03@gmail.com>
2024-11-02 21:17:30 +0200

    Added arrays exercises

---
+++ Arrays Exercise/Homework/Homework_one/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Задача
+
+int N;
+do
+{
+    Console.Write("Enter number N: ");
+    N = int.Parse(Console.ReadLine());
+} while (!(N <= 0 || N < 20));
+
+double[] array = new double[N];
+double sum = 0;
+
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"Enter number in the array {i + 1}: ");
+    array[i] = double.Parse(Console.ReadLine());
+    sum += array[i];
+}
+
+Console.WriteLine($"Sum of the first {N} elements is: {sum}");
